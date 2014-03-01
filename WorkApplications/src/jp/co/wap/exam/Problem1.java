ca0ac8d5977e3f8584ae549e389c31910e1a5689
Linn Htet Oo <lhtetoo@gmail.com>
2014-03-01 21:26:14 +0800

    improved version
    previous one has 15 IFs for figure 1 and 34 IFs for figure 2.
    this one has 12 IFs for figure 1 and 31 IFs for figure 2.

--- WorkApplications/src/jp/co/wap/exam/Problem1.java
+++ WorkApplications/src/jp/co/wap/exam/Problem1.java
@@ -24,62 +24,69 @@ public class Problem1 {
             return 0;
         }else {
             for (int i = 0; i < intervals.size(); i++){
-                System.out.println("first for: " + intervals.get(i).toString());
-                if(intervals.get(i) == null){
-                    return 0;
-                }else if((curCount + intervals.size() - i) <= maxCount){
-                    System.out.println("exit first for");
+                curCount = 0;
+                
+                if((curCount + intervals.size() - i) <= maxCount){                    
                     break;
                 }else{
-                    currentInterval = intervals.get(i);
-                    System.out.print("for " + currentInterval.toString());
-                    
-                    // get next intervals to compare starting from (i + 1) to (size - 1)
-                    for (int j = (i + 1); j < intervals.size(); j++){
-                        curCount = 0;
-                
-                        if(intervals.get(j) == null){
-                            return 0;
-                        }else if((curCount + intervals.size() - j) <= maxCount){
-                            break;
-                        }else{
-                            intervalToCompare = intervals.get(j);
-                            System.out.println(" and " + intervalToCompare.toString());
-                            // find overlapInterval between currentInterval and intervalToCompare
-                            overlapInterval = check.getOverlapInterval(currentInterval, intervalToCompare);
-                            if (overlapInterval != null){
-                                // increment maxCount
-                                curCount = 2;
-                                System.out.println("curCount: " + curCount);
-                                
-                                // find other intervals overlapping with overlapInterval
-                                for (int k = (j + 1); k < intervals.size(); k++){
-                                    if(intervals.get(k) == null){
-                                        return 0;
-                                    }else if((curCount + intervals.size() - k) <= maxCount){
-                                        break;
-                                    }else{
-                                        System.out.println(overlapInterval.toString() + "---" + intervals.get(k).toString());
-                                        if(check.isOverlapped(overlapInterval, intervals.get(k))){
-                                            curCount++;
-                                            System.out.println("curCount (in): " + curCount);                                
+                    if(intervals.get(i) == null){
+                        return 0;
+                    }else{
+                        currentInterval = intervals.get(i);
+
+                        // get next intervals to compare starting from (i + 1) to (size - 1)
+                        for (int j = (i + 1); j < intervals.size(); j++){
+                            curCount = 0;
+                            
+                            if((curCount + intervals.size() - j) <= maxCount){
+                                break;
+                            }else{
+                                if(intervals.get(j) == null){
+                                    return 0;
+                                }else{
+                                    intervalToCompare = intervals.get(j);
+                                    System.out.println(" and " + intervalToCompare.toString());
+                                    // find overlapInterval between currentInterval and intervalToCompare
+                                    overlapInterval = check.getOverlapInterval(currentInterval, intervalToCompare);
+                                    if (overlapInterval != null){
+                                        // increment maxCount
+                                        curCount = 2;
+                                        System.out.println("curCount: " + curCount);
+
+                                        // find other intervals overlapping with overlapInterval
+                                        for (int k = (j + 1); k < intervals.size(); k++){
+                                            if((curCount + intervals.size() - k) <= maxCount){
+                                                break;
+                                            }else{
+                                                if(intervals.get(k) == null){
+                                                    return 0;
+                                                }else{
+                                                    System.out.println(overlapInterval.toString() + "---" + intervals.get(k).toString());
+                                                    if(check.isOverlapped(overlapInterval, intervals.get(k))){
+                                                        curCount++;
+                                                        System.out.println("curCount (in): " + curCount);                                
+                                                        exeCount++;
+                                                    }
+                                                }
+                                                exeCount++;
+                                            }
+                                            exeCount++;
+                                        }// end 3rd for   
+                                        if (curCount > maxCount){
+                                            maxCount = curCount;
                                             exeCount++;
                                         }
-                                    }
-                                    exeCount++;
-                                }// end 3rd for   
-                                
-                                if (curCount > maxCount){
-                                    maxCount = curCount;
-                                    exeCount++;
+                                        System.out.println("maxCount: " + maxCount);
+                                        exeCount++;
+                                    }                             
                                 }
-                                System.out.println("maxCount: " + maxCount);
                                 exeCount++;
-                            }                             
-                        }
-                        exeCount++;
-                    }// end 2nd for
-                }
+                            }                                
+                            exeCount++;
+                        }// end 2nd for
+                    }
+                    exeCount++;
+                }                
                 exeCount++;
             }// end 1st for         
             System.out.println("after first for");
